21227c22a6a4e5eaef9e35b725cce26d9ed40deb
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-08-30 14:44:51 -0400

    Refactorización con updated_screen

--- settings/functions.py
+++ settings/functions.py
@@ -1,9 +1,20 @@
 import sys
 import pygame
 
+
 def check_controller():
     # Watch for keyboard and mouse events.
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
+
+def updated_screen(configuration, screen, warrior):
+    # Update the screen during each pass through the loop.
+    
+    # Redraw the screen during each pass through the loop with the function bg_screen() with the warrior.bmp
+    screen.fill(configuration.bg_screen)
+    warrior.start_on_screen()
+
+    # Make the most recently drawn screen visible. 
+    pygame.display.flip()
