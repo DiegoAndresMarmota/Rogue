85f6b3f8f1fcff4acbce1f99e5b291e9a7a94725
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-09-06 14:57:37 -0300

    Estructuración de movimiento en x e y

--- warrior.py
+++ warrior.py
@@ -3,8 +3,11 @@ import pygame
 #Create a class for the warrior
 class Warrior():
     #Iniciate the warrior and establish him position on the screen
-    def __init__(self, screen):
+    def __init__(self, screen, Configurations):
         self.screen = screen
+        
+        self.configurations = Configurations()
+        
         #Upload the image of the warrior and manipulate the image on the screen
         self.image = pygame.image.load('images/warrior.bmp')
         self.rect = self.image.get_rect()
@@ -20,21 +23,26 @@ class Warrior():
         self.moving_left = False
         self.moving_up = False
         self.moving_down = False
+        
+        #Center the warrior with float value
+        self.center_x = float(self.rect.centerx)
+        self.center_y = float(self.rect.centery)
     
     #Update the movement of the warrior
     def init_translation_movement(self):
         if self.moving_right:
-            self.rect.centerx += 1  
+            self.center_x += self.configurations.warrior_speed
         
         if self.moving_left:
-            self.rect.centerx -= 1
+            self.center_x -= self.configurations.warrior_speed
             
         if self.moving_up:
-            self.rect.centery += 1
+            self.center_y -= self.configurations.warrior_speed
             
         if self.moving_down:
-            self.rect.centery -= 1
+            self.center_y += self.configurations.warrior_speed
             
+        self.rect.center = self.center_x, self.center_y
     
     def start_on_screen(self):
         #Draw the warrior on the screen
